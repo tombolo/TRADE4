930bc065d3bef401267dc960d71350534ac0595f
Joseph Mumo <72470955+tombolo@users.noreply.github.com>
2026-01-27 20:52:46 +0300

    Remove social media links from default header
    Removed social media links from the header component.

--- packages/core/src/App/Containers/Layout/header/default-header.tsx
+++ packages/core/src/App/Containers/Layout/header/default-header.tsx
@@ -112,20 +112,7 @@ const DefaultHeader = observer(() => {
                                         current_language={current_language}
                                     />
                                 )}
-                                <div className='header__socials'>
-                                    <a href='' target='_blank' rel='noopener noreferrer' aria-label='Facebook'>
-                                        <Icon icon='IcFacebook' width={20} height={20} />
-                                    </a>
-                                    <a href='http://wa.me/254753668073' target='_blank' rel='noopener noreferrer' aria-label='WhatsApp'>
-                                        <Icon icon='IcWhatsappFilled' width={20} height={20} />
-                                    </a>
-                                    <a href='https://t.me/finesttradershub' target='_blank' rel='noopener noreferrer' aria-label='Telegram'>
-                                        <Icon icon='IcTelegram' width={20} height={20} />
-                                    </a>
-                                    <a href='https://www.instagram.com/finestburu?igsh=aWt2N2NnanZlYXhw' target='_blank' rel='noopener noreferrer' aria-label='Instagram'>
-                                        <InstagramIcon />
-                                    </a>
-                                </div>
+                               
                             </div>
                         </React.Fragment>
                     ) : (
